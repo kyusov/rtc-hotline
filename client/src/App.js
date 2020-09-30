ad67feae416c2e7abfd6c00265c6501e04554ee1
j3nqa <jeenjenya@gmail>
2020-09-30 13:39:26 +0300

    test

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer">
-          Learn React
+          Learn React J3nqa
         </a>
       </header>
     </div>
